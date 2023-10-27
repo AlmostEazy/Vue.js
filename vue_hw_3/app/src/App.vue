b40073ba7ee128e7b597668dd94ebb2df4d8d237
AlmostEazy <Antosha101992@rambler.ru>
2023-10-27 18:19:30 +0300

    feat: add vue hw files

---
+++ vue_hw_3/app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <ProductItems />
+</template>
+
+<script>
+import ProductItems from './components/ProductItems.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ProductItems
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ vue_hw_3/app/src/components/ProductItems.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <ProductDetails
+      v-for="product in products"
+      :key="product.id"
+      :product="product"
+    />
+  </div>
+</template>
+
+<script>
+import ProductDetails from "./ProductDetails.vue";
+
+export default {
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          name: "Pasta",
+          price: 1.55,
+          available: true,
+        },
+        {
+          id: 2,
+          name: "Milk",
+          price: 2.33,
+          available: false,
+        },
+        {
+          id: 3,
+          name: "Butter",
+          price: 0.99,
+          available: true,
+        },
+        {
+          id: 4,
+          name: "Meat",
+          price: 5.55,
+          available: false,
+        },
+      ],
+    };
+  },
+  components: {
+    ProductDetails,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
